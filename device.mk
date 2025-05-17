1d5cd2ce86dcf67237287e61049ac9cc591ad63f
Michael Bestas <mkbestas@lineageos.org>
2025-05-17 08:29:21 +0530

    sky: Move Lineage Health HAL to select() - Its deprecated
    vendor/lineage/config/BoardConfigSoong.mk:66: warning: TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS is deprecated, please migrate to soong_config_set,lineage_health,charging_control_supports_bypass
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- device.mk
+++ device.mk
@@ -271,6 +271,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
 
 # Lineage Health
+$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
+
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 

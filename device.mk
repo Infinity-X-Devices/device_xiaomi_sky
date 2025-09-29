9bd32c62049fc39093553055395f4a70f2ea793c
Lostark13 <tushar9915@gmail.com>
2025-09-29 12:28:30 +0000

    sky: Adjust soong configs

--- device.mk
+++ device.mk
@@ -271,10 +271,13 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
 
 # Lineage Health
-$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
-
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
+
+$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/qcom-battery/input_suspend)
+$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
+$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
 
 # NFC
 PRODUCT_COPY_FILES += \
